3f450d719fed2e391172562ccddfbdb93acfd6c3
dvidanage <dvidanage@gmail.com>
2026-03-19 09:50:20 +0530

    Create Room.js model file with mongoose schema

---
+++ backend/models/Room.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  features: [String],  // Array of features
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = Room;
